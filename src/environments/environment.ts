cbb7e3883c752dad5cadafcbb7309f1fbfca22f3
shivamshri05 <shrivastavashivam1302@gmail.com>
2025-03-04 01:27:45 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-rapidapi-endpoint.com',
+  apiUrl: 'https://cricbuzz-cricket.p.rapidapi.com',
   apiKey: '6f76cd915amshdea56ca8f6f621fp18c111jsnc1b12020e1c5'
 };
