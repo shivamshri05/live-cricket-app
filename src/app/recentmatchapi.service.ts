61ce6386291e764d90d92f955b92485d78cd8082
shivamshri05 <shrivastavashivam1302@gmail.com>
2025-03-04 22:56:34 +0530

    created two varibles in recentmatch.service

--- src/app/recentmatchapi.service.ts
+++ src/app/recentmatchapi.service.ts
@@ -7,16 +7,19 @@ import { environment } from '../environments/environment';
   providedIn: 'root'
 })
 export class RecentmatchapiService {
- private apiUrl = 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent '; 
+  RAPIDAPI_KEY:any
+  RAPIDAPI_HOST:any
+ private apiUrl = 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent/?RAPIDAPI_KEY&RAPIDAPI_HOST'; 
 
   constructor(private http: HttpClient) { }
 
   getRecentMatches(): Observable<any> {
+    
     const headers = {
       'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com', 
       'x-rapidapi-key': environment.apiKey 
     };
       console.log(headers);
-    return this.http.get(this.apiUrl, { headers });
+    return this.http.get(this.apiUrl);
   }
 }
