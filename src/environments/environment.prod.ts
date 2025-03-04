8c540ca2d1f52c74a65df6e439205d49880fb39a
shivamshri05 <shrivastavashivam1302@gmail.com>
2025-03-05 00:39:49 +0530

    api key env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export interface Environment {
   production: boolean;
   apiUrl: string;
-  apiKey: string;
+  apiKey: any;
   rapidApiHost: string;
 }
 
 export const environment: Environment = {
   production: false,
   apiUrl: 'https://cricbuzz-cricket.p.rapidapi.com',
-  apiKey: process.env['RAPIDAPI_KEY'] || '',
+  apiKey: process.env['RAPIDAPI_KEY'],
   rapidApiHost: 'cricbuzz-cricket.p.rapidapi.com'
 };
