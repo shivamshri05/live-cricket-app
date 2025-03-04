6f56bed8ace43fcfddc5ed756e3ebedb90c98bbe
shivamshri05 <shrivastavashivam1302@gmail.com>
2025-03-05 00:51:25 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ declare global {
 }
 
 export const environment: Environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://cricbuzz-cricket.p.rapidapi.com',
   apiKey: window['process']?.env?.RAPIDAPI_KEY || '6f76cd915amshdea56ca8f6f621fp18c111jsnc1b12020e1c5',
   rapidApiHost: window['process']?.env?.RAPIDAPI_HOST || 'cricbuzz-cricket.p.rapidapi.com'
